b43b33326e1e311229bdb0a552017a9fd504adf3
Andrew Hannigan <ahannigan@cogolabs.com>
2012-07-12 11:57:16 -0400

    added switching and regex arg matching

--- background.js
+++ background.js
@@ -1,9 +1,34 @@
 ws = /\s+/
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
+    
+    // Input formatting
     args = text.split(ws);
     args = args.filter(function(x){return x!="";})
-    navigate("http://maps.google.com/maps?q=" + text);
+    if (args.length > 1) {
+      temp = args;
+      tail = temp.splice(1).join(" ");
+    }
+    else {
+      tail = "";
+    }
+    
+
+    // Switches
+    var isMap = RegExp("^ma*ps*$", "i");
+    if (isMap.test(args[0])) {
+      navigate("http://maps.google.com/maps?q=" + tail);
+    }
+
+    var isCal = RegExp("(^cal$|^cale?n?d?(a|e)?r?$)", "i");
+    if (isCal.test(args[0])) {
+      alert("yay cal") 
+    }
+
+    else {
+      alert("CL DOES NOT UNDERSTAND THIS NONSENSE:\n" + "'" + text + "'");
+    }
+    
 });
 
 
